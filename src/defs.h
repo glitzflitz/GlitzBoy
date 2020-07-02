bad1ed483374a7aee30bd06cddb091ede524deb8
Amey Narkhede <narkhedeamey11@gmail.com>
2020-07-02 13:11:37 +0530

    Added typedefs

---
+++ src/defs.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef unsigned u32;
+typedef uint_fast8_t uf8;
+typedef uint_fast16_t uf16;
+typedef uint_fast32_t uf32;
+typedef int_fast16_t if16;
+typedef float f32;
