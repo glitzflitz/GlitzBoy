809706a32953bacef0699fe3afb7c76f2d4900a6
Amey Narkhede <narkhedeamey11@gmail.com>
2020-07-05 17:34:03 +0530

    Defined core structs

--- src/gb.h
+++ src/gb.h
@@ -101,3 +101,225 @@ struct Gameboy;
 #define LCD_PALETTE_OBJ 0x10
 #define LCD_PALETTE_BG 0x20
 #define LCD_PALETTE_ALL 0x30
+
+typedef struct Timer
+{
+	uf16 lcd_count;
+	uf16 div_count;
+	uf16 tima_count;
+	uf16 serial_count;
+} Timer;
+
+typedef struct Registers
+{
+	union
+	{
+		struct
+		{
+			union
+			{
+				struct
+				{
+					u8 padding : 4; /*Unused */
+					u8 C : 1;		/* Carry flag. */
+					u8 H : 1;		/* Half carry flag. */
+					u8 N : 1;		/* Sign/Negative flag. */
+					u8 Z : 1;		/* Zero flag. */
+				} raw_bits;
+				u8 f;
+			};
+			u8 a;
+		};
+		u16 AF;
+	};
+
+	union
+	{
+		struct
+		{
+			u8 C;
+			u8 B;
+		};
+		u16 BC;
+	};
+
+	union
+	{
+		struct
+		{
+			u8 E;
+			u8 D;
+		};
+		u16 DE;
+	};
+
+	union
+	{
+		struct
+		{
+			u8 L;
+			u8 H;
+		};
+		u16 HL;
+	};
+
+	u16 SP;
+	u16 PC;
+} Registers;
+
+typedef struct hw_registers
+{
+	u8 TIMA, TMA, DIV;
+	union
+	{
+		struct
+		{
+			u8 rate : 2;
+			u8 enable : 1;
+			u8 padding : 5;
+		};
+		u8 TAC;
+	};
+
+	/* LCD */
+	u8 LCDC;
+	u8 STAT;
+	u8 SCY;
+	u8 SCX;
+	u8 LY;
+	u8 LYC;
+	u8 DMA;
+	u8 BGP;
+	u8 OBP0;
+	u8 OBP1;
+	u8 WY;
+	u8 WX;
+
+	u8 P1;
+
+	u8 SB;
+	u8 SC;
+
+	u8 IF;
+
+	u8 IE;
+} hw_registers;
+
+typedef struct Display
+{
+	void (*gpu_draw_line)(struct Gameboy *,
+						  const u8 pixels[static 160],
+						  const uf8 line);
+
+	u8 bg_palette[4];
+	u8 sp_palette[8];
+
+	u8 window_clear;
+	u8 WY;
+
+	u32 frame_skip_count : 1;
+	u32 interlace_count : 1;
+} Display;
+
+enum Error
+{
+	UNKNOWN_ERROR,
+	INVALID_OPCODE,
+	INVALID_READ,
+	INVALID_WRITE,
+
+	INVALID_MAX
+};
+
+enum InitError
+{
+	INIT_NO_ERROR,
+	INIT_CARTRIDGE_UNSUPPORTED,
+	INIT_INVALID_HASH
+};
+
+enum SerialStatus
+{
+	SERIAL_SUCCESS = 0,
+	SERIAL_NO_CONNECTION = 1
+};
+
+typedef struct Gameboy
+{
+	u8 (*read_rom)(struct Gameboy *, const uint_fast32_t address);
+
+	u8 (*read_ram)(struct Gameboy *, const uint_fast32_t address);
+
+	void (*write_ram)(struct Gameboy *, const uint_fast32_t address,
+					  const u8 value);
+
+	void (*Error)(struct Gameboy *, const enum Error, const u16 value);
+
+	u8 (*serial_transmit)(struct Gameboy *, const u8 tx);
+	enum SerialStatus (*serial_recv)(struct Gameboy *, u8 *rx);
+
+	struct
+	{
+		unsigned char halt : 1;
+		unsigned char ime : 1;
+		unsigned char frame : 1;
+
+		unsigned char lcd_mode : 2;
+	};
+
+	u8 mbc;
+	u8 cartridge_ram;
+	u16 num_rom_banks;
+	u8 num_ram_banks;
+
+	u8 selected_rom_bank;
+	u8 cart_ram_bank;
+	u8 enable_cart_ram;
+	u8 cart_mode_select;
+	union
+	{
+		struct
+		{
+			u8 sec;
+			u8 min;
+			u8 hour;
+			u8 yday;
+			u8 high;
+		} raw_bits;
+		u8 cart_rtc[5];
+	};
+
+	Registers cpu_reg;
+	struct hw_registers hw_reg;
+	Timer timer;
+	Display display;
+
+	u8 wram[WRAM_SIZE];
+	u8 vram[VRAM_SIZE];
+	u8 hram[HRAM_SIZE];
+	u8 oam[OAM_SIZE];
+
+	struct
+	{
+		u32 interlace : 1;
+		u32 skipframe : 1;
+
+		union
+		{
+			struct
+			{
+				u32 a : 1;
+				u32 b : 1;
+				u32 select : 1;
+				u32 start : 1;
+				u32 right : 1;
+				u32 left : 1;
+				u32 up : 1;
+				u32 down : 1;
+			} raw_bits;
+			u8 joypad;
+		};
+
+		void *misc_data;
+	} direct;
+} Gameboy;
